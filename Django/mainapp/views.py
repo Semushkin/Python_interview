9f436313c0c6c63196f8218644fdb104ce677f40
anton <rkzton@yandex.ru>
2023-04-23 17:21:31 +0300

    lesson_5

--- Django/mainapp/views.py
+++ Django/mainapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.urls import reverse
 from .forms import ProductForm
 from .models import Product
@@ -14,15 +15,28 @@ def index(request):
 
 def add_product(request):
     if request.method == 'POST':
-        product_form = ProductForm(data=request.POST)
-        if product_form.is_valid():
-            product_form.save()
-        return HttpResponseRedirect(reverse('mainapp:index'))
+        form = ProductForm(data=request.POST)
+        template_name = 'mainapp/goods_list.html'
     else:
-        context = {
-            'product_form': ProductForm()
-        }
-        return render(request, 'mainapp/good_create.html', context)
+        form = ProductForm()
+        template_name = 'mainapp/good_create.html'
+    return save_good_form(request, form, template_name)
+
+
+def save_good_form(request, form, template_name):
+    data = dict()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            data['html_good_list'] = render_to_string('mainapp/goods_list.html', {'products': Product.objects.all()})
+        else:
+            data['form_is_valid'] = False
+            print(f'Ошибка валидации формы: {form.errors}')
+    else:
+        context = {'form': form}
+        data['html_form'] = render_to_string(template_name, context, request=request)
+    return JsonResponse(data)
 
 
 def edit_product(request, product_pk: int):
